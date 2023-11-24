a17b7b3a842518408cd2c8c5290e96cbb90a4fd5
Alex Petrenko <alex.pt@ya.ru>
2023-11-24 13:07:24 +0300

    task #2 (делимость на 3) completed

--- ErrorNilUnit1Homeworks/ErrorNilUnit1Homeworks/main.swift
+++ ErrorNilUnit1Homeworks/ErrorNilUnit1Homeworks/main.swift
@@ -2,18 +2,6 @@ import Foundation
 
 // MARK: - Task 1. Четность числа
 
-/*
-   - *Описание:* 
- Разработайте функцию, которая определяет, является ли предоставленное число четным.
- 
-   - *Требования:*
-     - Функция определения: Создайте функцию, которая принимает число в качестве параметра.
-     - Функция должна возвращать true, если число четное, и false, если нечетное.
- 
-   - *Ожидаемый результат:*
- При передаче числа в функцию, она вернет результат четности числа.
-*/
-
 // вариант 1:
 func numberEvenOrOdd(_ num: Int) -> Bool {
     num.isMultiple(of: 2)
@@ -31,3 +19,23 @@ func numberEvenOrOdd2(_ num: Int) -> Bool {
 }
 
 print(numberEvenOrOdd2(5))
+
+// MARK: - Task 2. Делимость на 3
+
+// вариант 1:
+func divisionByThree(_ num: Int) -> Bool {
+    num.isMultiple(of: 3)
+}
+
+print(divisionByThree(11))
+
+// вариант 2:
+func divisionByThree2(_ num: Int) -> Bool {
+    if num % 3 == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+print(divisionByThree2(12))
