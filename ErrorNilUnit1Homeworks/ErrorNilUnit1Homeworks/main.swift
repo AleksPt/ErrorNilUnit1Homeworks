d3c5e185b4ae6f2570388e9eca0a8077bdcdacb5
Alex Petrenko <alex.pt@ya.ru>
2023-12-05 18:06:26 +0300

    task 1

--- ErrorNilUnit1Homeworks/ErrorNilUnit1Homeworks/main.swift
+++ ErrorNilUnit1Homeworks/ErrorNilUnit1Homeworks/main.swift
@@ -1,3 +1,28 @@
 import Foundation
 
+// MARK: - 1 задание. Времена года и их температура
+
+enum Season {
+    case winter
+    case spring
+    case summer
+    case fall
+    
+    var averageTemperature: Double {
+        switch self {
+        case .winter:
+            return -6.3
+        case .spring:
+            return 1.6
+        case .summer:
+            return 13.7
+        case .fall:
+            return 4.2
+        }
+    }
+}
+
+let resultTask1: Season = .summer
+resultTask1.averageTemperature
+
 
