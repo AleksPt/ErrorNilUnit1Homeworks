58039d604f54828a421f17638848d1d01b8e8dd8
Alex Petrenko <alex.pt@ya.ru>
2023-12-05 18:14:10 +0300

    task 2

--- ErrorNilUnit1Homeworks/ErrorNilUnit1Homeworks/main.swift
+++ ErrorNilUnit1Homeworks/ErrorNilUnit1Homeworks/main.swift
@@ -25,4 +25,18 @@ enum Season {
 let resultTask1: Season = .summer
 resultTask1.averageTemperature
 
+//MARK: - 2 задание. Информация о транспортных средствах
+
+enum Transport {
+   case car(Int)
+   case bicycle(Int)
+   case train(Int)
+   case plane(Int)
+}
+
+let car: Transport = .car(250)
+let bicycle: Transport = .bicycle(30)
+let train: Transport = .train(400)
+let plane: Transport = .plane(1000)
+
 
